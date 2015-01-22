fb75f0a0370ab459a650627c7b9dd131515fb7f9
Kybook <kyle@kammllc.com>
2015-01-21 21:02:23 -0500

    add destroy all

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Delayed::Web::Engine.routes.draw do
 
   resources :jobs, only: [:destroy, :index, :show] do
     put :queue, on: :member
+  end
+  namespace :job do 
     delete :destroy_all
   end
 end
